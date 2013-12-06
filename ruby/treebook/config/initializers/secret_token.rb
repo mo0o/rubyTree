15f4b1e4aef6846eaf6c6bc89bb8a3b414df5dc6
Richard Davies <rdavis30@gmail.com>
2013-12-06 14:08:54 +0000

    Add Bootstrap, and applied layout CSS

---
+++ ruby/treebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '4ba1982da18cef45e10f49902574b96bff4693a3c618e55fc39478579543a9951274e1fd6a3bf46c0e628598227e0685175ca6917a73edf9666e70cb328c93e9'
